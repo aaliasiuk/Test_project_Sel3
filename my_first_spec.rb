c2e4ea6e82c37c417e39af532fffbc874eb08ce6
aaliasiuk <aliasiuk13@gmail.com>
2017-02-27 23:48:32 -0800

    Changing to Litecart

--- my_first_spec.rb
+++ my_first_spec.rb
@@ -1,18 +1,19 @@
 require 'rspec'
 require 'selenium-webdriver'
 
-describe  'Google search' do
+describe  'Duck Search' do
   before(:each) do
     @driver = Selenium::WebDriver.for :firefox
     @wait = Selenium::WebDriver::Wait.new(:timeout => 10)
   end
-  it "should find webdriver " do
-    @driver.navigate.to 'http://google.com'
+  it "should find duck " do
+    @driver.navigate.to 'http://localhost/litecart'
 
-    element = @driver.find_element(:name, 'q')
-    element.send_keys 'webdriver'
-    @driver.find_element(:name, 'btnG').click
-    @wait.until {@driver.title == 'webdriver - Google Search'}
+    element = @driver.find_element(:name, 'query')
+    element.send_keys 'duck'
+    element.submit
+    #@driver.find_element(:name, 'btnG').click
+    @wait.until {@driver.title == 'Search Results for "duck" | My Store'}
   end
 
   after(:each ) do
